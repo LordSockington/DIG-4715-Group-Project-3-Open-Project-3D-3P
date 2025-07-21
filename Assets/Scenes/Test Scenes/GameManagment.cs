707f78b9cdb7c96a3f7d2deb252695bc66020441
LordSockington <142945619+LordSockington@users.noreply.github.com>
2025-07-21 14:16:11 -0400

    GameManagment
    Wrote GameManagment script to manage scenes.

---
+++ Assets/Scenes/Test Scenes/GameManagment.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class GameManagment : MonoBehaviour
+{
+    //Names game manager
+    public static GameManagment manager;
+
+    void Awake()
+    {
+        //If game manager does not exist, do this:
+        if (manager == null)
+        {
+            manager = this;
+            DontDestroyOnLoad(this);
+        }
+
+        //If game manager equals something other than this scrpit, do this:
+        else if (manager != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
